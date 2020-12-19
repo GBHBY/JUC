c31ab124cfd6303f0d21e8cc00e114f31e2d45aa
GBHBY <15776547151@163.com>
2020-12-19 17:05:14 +0800

    本类的目的是表明：根据juc特性，ReentrantLock锁住的任务阻塞的情况下，可以被中断， 在之前，只要任务以不可中断的方式被阻塞（例如IO和被synchronized修饰的方法），那么程序就有可能被锁住

---
+++ src/com/gyb/demo6/Interrupting2.java
@@ -0,0 +1,54 @@
+package com.gyb.demo6;//: concurrency/Interrupting2.java
+// Interrupting a task blocked with a ReentrantLock.
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+
+class BlockedMutex {
+  private Lock lock = new ReentrantLock();
+  public BlockedMutex() {
+    // Acquire it right away, to demonstrate interruption
+    // of a task blocked on a ReentrantLock:
+    lock.lock();
+  }
+  public void f() {
+    try {
+      // This will never be available to a second task
+      lock.lockInterruptibly(); // Special call
+      System.out.println("lock acquired in f()");
+    } catch(InterruptedException e) {
+      System.out.println("Interrupted from lock acquisition in f()");
+    }
+  }
+}
+
+class Blocked2 implements Runnable {
+  BlockedMutex blocked = new BlockedMutex();
+  public void run() {
+    System.out.println("Waiting for f() in BlockedMutex");
+    blocked.f();
+    System.out.println("Broken out of blocked call");
+  }
+}
+
+/**
+ * 本类的目的是表明：根据juc特性，ReentrantLock锁住的任务阻塞的情况下，可以被中断，
+ *  在之前，只要任务以不可中断的方式被阻塞（例如IO和被synchronized修饰的方法），那么程序就有可能被锁住
+ */
+public class Interrupting2 {
+  public static void main(String[] args) throws Exception {
+    Thread t = new Thread(new Blocked2());
+    t.start();
+    TimeUnit.SECONDS.sleep(1);
+    System.out.println("Issuing t.interrupt()");
+    t.interrupt();
+  }
+} /* Output:
+Waiting for f() in BlockedMutex
+Issuing t.interrupt()
+Interrupted from lock acquisition in f()
+Broken out of blocked call
+*///:~
