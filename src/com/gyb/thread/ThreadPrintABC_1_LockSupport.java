eb4c45391a9bb17d15298dc70901a36f7f2cb70f
guoyi <15776547151@163.com>
2023-09-02 16:11:25 +0800

    增加lockSupport实现顺序打印

---
+++ src/com/gyb/thread/ThreadPrintABC_1_LockSupport.java
@@ -0,0 +1,48 @@
+package com.gyb.thread;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * Description:
+ *
+ * @author GB
+ * @date 2023/7/20 16:31
+ */
+public class ThreadPrintABC_1_LockSupport {
+
+    private static Thread threadA, threadB, threadC;
+
+    public static void main(String[] args) {
+        threadA = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                // 打印当前线程名称
+                System.out.print(Thread.currentThread().getName());
+                // 唤醒下一个线程
+                LockSupport.unpark(threadB);
+                // 当前线程阻塞
+                LockSupport.park();
+            }
+        }, "A");
+        threadB = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                // 先阻塞等待被唤醒
+                LockSupport.park();
+                System.out.print(Thread.currentThread().getName());
+                // 唤醒下一个线程
+                LockSupport.unpark(threadC);
+            }
+        }, "B");
+        threadC = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                // 先阻塞等待被唤醒
+                LockSupport.park();
+                System.out.print(Thread.currentThread().getName());
+                // 唤醒下一个线程
+                LockSupport.unpark(threadA);
+            }
+        }, "C");
+        threadA.start();
+        threadB.start();
+        threadC.start();
+    }
+}
